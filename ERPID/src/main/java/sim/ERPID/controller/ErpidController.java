baea58934ce31bef397411ca693d06089197abae
SeongYunSim <jghee717@naver.com>
2020-08-27 06:06:07 +0900

    회원 초기 테스트

--- ERPID/src/main/java/sim/ERPID/controller/ErpidController.java
+++ ERPID/src/main/java/sim/ERPID/controller/ErpidController.java
@@ -3,6 +3,8 @@ package sim.ERPID.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ErpidController {
@@ -11,5 +13,36 @@ public class ErpidController {
     public String Erpid(Model model){
         model.addAttribute("data","Spring");
         return "Erpid";
+    }
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello "+name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello{
+        private String name;
+
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
     }
 }
