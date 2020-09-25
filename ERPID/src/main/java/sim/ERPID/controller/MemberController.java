ec0eb8418d66c701311c99728bd3646c07ec1495
SeongYunSim <jghee717@naver.com>
2020-09-25 09:14:41 +0900

    테이블, 정보 확

--- ERPID/src/main/java/sim/ERPID/controller/MemberController.java
+++ ERPID/src/main/java/sim/ERPID/controller/MemberController.java
@@ -28,6 +28,7 @@ public class MemberController {
         return "members/signup";
     }
 
+
     @PostMapping(value = "/members/new")
     public String create(MemberForm form) {
         Member member = new Member();
@@ -52,6 +53,33 @@ public class MemberController {
         List<Member> members = memberService.findMembers();
         model.addAttribute("members", members);
         return "members/memberList";
+    }
+
+
+    @PostMapping(value = "/members/dd")
+    public String create2(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setNumber(form.getNumber());
+        member.setPw(form.getPw());
+        member.setEmail(form.getEmail());
+        member.setSex(form.getSex());
+        member.setAddress(form.getAddress());
+        member.setJumin(form.getJumin());
+        member.setDepartment(form.getDepartment());
+        member.setPosition(form.getPosition());
+        member.setStatus(form.getStatus());
+        member.setHire(form.getHire());
+        member.setResign(form.getResign());
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members/dd")
+    public String list2(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/dd";
     }
 
 }
