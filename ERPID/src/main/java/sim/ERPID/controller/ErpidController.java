cfea7d0841c5f317e34130b8799a4236d37ffd09
SeongYunSim <jghee717@naver.com>
2020-11-09 01:16:57 +0900

    home -> index

--- ERPID/src/main/java/sim/ERPID/controller/ErpidController.java
+++ ERPID/src/main/java/sim/ERPID/controller/ErpidController.java
@@ -14,25 +14,7 @@ public class ErpidController {
         model.addAttribute("data","Spring");
         return "Erpid";
     }
-    @GetMapping("hello-mvc")
-    public String helloMvc(@RequestParam("name") String name, Model model){
-        model.addAttribute("name",name);
-        return "hello-template";
-    }
 
-    @GetMapping("hello-string")
-    @ResponseBody
-    public String helloString(@RequestParam("name") String name){
-        return "hello "+name;
-    }
-
-    @GetMapping("hello-api")
-    @ResponseBody
-    public Hello helloApi(@RequestParam("name") String name){
-        Hello hello = new Hello();
-        hello.setName(name);
-        return hello;
-    }
     static class Hello{
         private String name;
 
