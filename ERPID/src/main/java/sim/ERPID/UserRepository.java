7a2031ea5151ab911fd51fe7db32eadb40ce5891
SeongYunSim <jghee717@naver.com>
2021-02-02 00:50:02 +0900

    ?

--- ERPID/src/main/java/sim/ERPID/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package sim.ERPID;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<UserInfo, Long> {
-    Optional<UserInfo> findByEmail(String email);
-}
